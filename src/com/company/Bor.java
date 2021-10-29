e470097f9e3c2faacd24604e9348d9a9b1bf6729
Erinad-2002 <Erinad.business@gmail.com>
2021-10-29 18:22:31 +0200

    bor class finished

--- src/com/company/Bor.java
+++ src/com/company/Bor.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class Bor {
+
+    private String fajta;
+    private int evjarat;
+
+    public Bor(String fajta, int evjarat) {
+        this.fajta = fajta;
+        this.evjarat = evjarat;
+    }
+
+    public String getFajta() {
+        return fajta;
+    }
+
+    public int getEvjarat() {
+        return evjarat;
+    }
+
+    public void setFajta(String fajta) {
+        this.fajta = fajta;
+    }
+
+    public void setEvjarat(int evjarat) {
+        this.evjarat = evjarat;
+    }
+
+    @Override
+    public String toString() {
+        return "A bor fajtája: " +fajta+ ", évjárata: " +evjarat;
+    }
 }
