d5620e4e1987abe0ec9ab6cf89c3435347bc7966
Erinad-2002 <Erinad.business@gmail.com>
2021-10-29 18:44:12 +0200

    test class finished

---
+++ src/com/company/TesztBor.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class TesztBor {
+
+    public static void main(String[] args) {
+        Bor b1 = new Bor("Voros", 1999);
+        Bor b2 = new Bor("Feher", 2010);
+        Bor b3 = new Aszu(1995, 4);
+
+        kiirBor(b1);
+        kiirBor(b2);
+        kiirBor(b3);
+    }
+
+    public static void kiirBor(Bor bor) {
+        System.out.println(bor);
+    }
+}
